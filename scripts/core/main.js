6bb4871e504910fba881094c2578f905f67cf798
PRINCIPIA <AlexMindstorms@hotmail.com>
2026-04-28 11:15:59 +1200

    Added high score functionality

--- scripts/core/main.js
+++ scripts/core/main.js
@@ -8,6 +8,13 @@
 
 const HTML_OUTPUT = document.getElementById("databaseOutput");
 
+const HTML_USERNAME = document.getElementById("i_username");
+const HTML_HIGH_SCORE = document.getElementById("i_hiscore");
+const HIGH_SCORE_LIST = "high-scores/";
+
+const HTML_HIGH_SCORE_O_SINGLE = document.getElementById("o_single_hiscore");
+const HTML_HIGH_SCORE_TABLE = document.getElementById("o_hiscore_table");
+
 
 initialize();
 
@@ -15,7 +22,6 @@ initialize();
 //initialize()
 function initialize() {
     fb_init();
-    fb_addWriteListener('messages/message', cb_updateDatabaseText);
 }
 //------------------------------------------------------------------------------//
 
@@ -30,3 +36,119 @@ function cb_updateDatabaseText(val) {
     HTML_OUTPUT.innerHTML = val.val();
 }
 //------------------------------------------------------------------------------//
+
+
+
+//------------------------------------------------------------------------------//
+//                                 High Scores                                  //
+//------------------------------------------------------------------------------//
+
+
+//------------------------------------------------------------------------------//
+//addHighScore(name, score)
+function addHighScore(name, score) {
+    console.log("addHighScore(name, score) :: adding " + name + "'s high score of " + score);
+    fb_write(HIGH_SCORE_LIST + name, "", score);
+}
+//------------------------------------------------------------------------------//
+
+
+//------------------------------------------------------------------------------//
+//addHighScoreFromDocument()
+//gets the high score and username from html input boxes
+function addHighScoreFromDocument() {
+    const SCORE = HTML_HIGH_SCORE.value;
+    const USERNAME = HTML_USERNAME.value;
+
+    addHighScore(USERNAME, SCORE);
+}
+//------------------------------------------------------------------------------//
+
+
+//------------------------------------------------------------------------------//
+//getHighScore(name)
+async function getHighScore(name) {
+    var score = await fb_read(HIGH_SCORE_LIST + "/" + name);
+    
+    return score;
+}
+//------------------------------------------------------------------------------//
+
+
+//------------------------------------------------------------------------------//
+//displaySingleHighScore(name)
+async function displaySingleHighScore(name) {
+    const SCORE = await getHighScore(name);
+
+    const TEXT = "Name: " + name + ", high score: " + SCORE;
+
+    HTML_HIGH_SCORE_O_SINGLE.innerHTML = TEXT;
+}
+//------------------------------------------------------------------------------//
+
+
+//------------------------------------------------------------------------------//
+//displayHighScoreFromDocument()
+async function displayHighScoreFromDocument() {
+    const NAME = HTML_USERNAME.value;
+    await displaySingleHighScore(NAME);
+}
+//------------------------------------------------------------------------------//
+
+
+
+//------------------------------------------------------------------------------//
+//displayHighScoreTable()
+async function displayHighScoreTable() {
+    var obj = null;
+    await fb_read(HIGH_SCORE_LIST, (value)=>{displayTable(value.val());});
+
+    function displayTable(json) {
+        if (json == null) {
+            console.error("displayHighScoreTable() :: read was null");
+            return;
+        }
+        const TEXT = JSON.stringify(json);
+
+        const SPLIT_TEXT_ARR = TEXT.split(','); //Split into key - value pairs
+
+        var html_table_text = "";
+
+        //Add table tag
+        html_table_text += "<table>";
+
+        //Add header
+        html_table_text += "<tr><th>Name</th><th>Score</th></tr>"
+
+        for (var i = 0; i < SPLIT_TEXT_ARR.length; i++){
+            var data = SPLIT_TEXT_ARR[i];
+            //Remove brackets
+            data = data.replaceAll('{', '');
+            data = data.replaceAll('}', '');
+
+            //Data is now "key":value
+
+            //Remove "" from around keys
+            data = data.replaceAll('"', '');
+
+            //Data is now key:value
+
+            //Split by colon
+            const KEY_VAL_PAIR = data.split(':');
+
+            //Element 0 is the key
+            //Element 1 is the value
+
+            const KEY = KEY_VAL_PAIR[0]; //Username
+            const VAL = KEY_VAL_PAIR[1]; //Score
+
+            html_table_text += "<tr><td>" + KEY + "</td><td>" + VAL + "</td></tr>";
+        }
+
+        //Add closing table tag
+        html_table_text += "</table>";
+
+        HTML_HIGH_SCORE_TABLE.innerHTML = html_table_text;
+    }
+    
+}
--- scripts/firebase/fb_io.js
+++ scripts/firebase/fb_io.js
@@ -9,34 +9,44 @@
 
 
  //------------------------------------------------------------------------------//
- //fb_write(msg)
+ //fb_write(path, msg)
+ //path: the path to write to
+ //key: the key to the message
  //msg: the message to write
- function fb_write(path, msg){
-    console.log("fb_write(path, msg)\npath = " + path + "\nmsg = " + msg);
+ function fb_write(path, key, msg){
+    console.log("fb_write(path, msg)\npath = '" + path + "'\nmsg = " + msg);
 
-    //Avoid writing to database root
+    //Avoid writing to database root and deleting everything
     if (path == "/") {
-        console.warn("fb_write(path, msg) :: attempted to write " + msg + " to the database root.");
-        console.log("fb_write(path, msg) :: attempted to write to database root, returning early.");
+        console.error("fb_write(path, msg) :: attempted to write " + msg + " to the database root.");
+        console.warn("fb_write(path, msg) :: attempted to write to database root, aborting");
         return;
     }
+
+    if (key == "") {
+        //We are just writing a value to a list
+        firebase.database().ref(path).set(msg);
+        return;
+    } else {
+        //We are writing a value with an explicitly defined key
+        const JSON_STRING = '{"' + key + '": "' + msg + '"}';
+        const JSON_OBJECT = JSON.parse(JSON_STRING);
+        
+        firebase.database().ref(path + "/" + key).set(JSON_OBJECT);
+    }
     
-    firebase.database().ref(path).set(
-        {
-        message: msg
-        }
-    );
 }
 //------------------------------------------------------------------------------//
 
 
-
+//Ensure that only one read can happen at a time
+//Stores a boolean specifying if the current path can be read (true = yes)
 var reads = [];
 
 //------------------------------------------------------------------------------//
 //fb_read(path, cb)
-async function fb_read(path, cb) {
-    console.log("fb_read(path, cb)\npath = " + path);
+async function fb_read(path, cb = ()=>{}) {
+    console.log("fb_read(path, cb)\npath = '" + path + "'");
 
     if (!reads.includes(path)) {
         reads[path] = true
@@ -45,16 +55,46 @@ async function fb_read(path, cb) {
     while (!reads[path]){}
     console.log("fb_read(path, cb) :: read access gained");
     reads[path] = false;
-    firebase.database().ref(path).once('value', (val)=>{reads[path] = true; cb(val);});
-    
+    if (cb.toString() != (()=>{}).toString()) {
+        //The user is handling the data
+        firebase.database().ref(path).once('value', (val)=>{reads[path] = true; cb(val);});
+    } else {
+        //We must handle (return) the data
+        reads[path] = true;
+        return (await firebase.database().ref(path).get()).val();
+    }
 }
 //------------------------------------------------------------------------------//
 
+//One unique listener per path
+var listenerPaths = [];
+var listenerCBs = [];
 
 //------------------------------------------------------------------------------//
 //fb_addWriteListener(path, cb)
 function fb_addWriteListener(path, cb) {
-    console.log("fb_addWriteListener(path, cb)\npath = " + path);
+    console.log("fb_addWriteListener(path, cb)\npath = '" + path + "'");
+
+    var isNewPath = false;
+    var isNewCb = false;
+
+    if (!listenerPaths.includes(path)) {
+        listenerPaths.push(path);
+        isNewPath = true;
+    }
+
+    //We must use toString to allow for different COPIES (not references) of the same callback code
+    if (!listenerCBs.includes(cb.toString())) {
+        listenerCBs.push(cb.toString());
+        isNewCb = true;
+    }
+    if (!isNewPath && !isNewCb) {
+        //The exact same write listener (same path, same callback) already exists
+        console.error("fb_addWriteListener(path, cb) :: there is already a write listener at path '" + path + "' with the same callback.");
+        console.warn("fb_addWriteListener(path, cb) :: attempted to add a duplicate write listener, aborting.");
+        return;
+    }
+    
     firebase.database().ref(path).on('value', cb);
 }
 //------------------------------------------------------------------------------//
